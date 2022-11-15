1057fd393378756ed38585cc2aedb6731a46ccb3
Lara Raffel <lara.b.r@outlook.com>
2022-11-14 22:48:14 -0300

    Primeiro commit realizado. Classe Impressora e classe CalculadoraCientifica criadas.

---
+++ CalculadoraCientifica.java
@@ -0,0 +1,27 @@
+public class CalculadoraCientifica{
+    
+    public double raiz(int a){
+        return Math.sqrt(a);
+    }
+
+    public double raiz(double a){
+        return Math.sqrt(a);
+    }
+
+    public double raiz(String a){
+        return Math.sqrt(Double.parseDouble(a));
+    }
+
+    public double potencia(byte a, byte b) {
+        return Math.pow(a, b);
+    }
+
+    public double potencia(String s1, String s2) {
+        return Math.pow(Double.parseDouble(s1), Double.parseDouble(s2));
+    }
+
+    public double potencia(int a, double b) {
+        return Math.pow(a, b);
+
+    }
+}
